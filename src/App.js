397a88b123501330b46273d0931b7266c4f201a1
Yuriy <yuriy.gavrilyuk@auroraglobal.com>
2018-11-12 18:42:41 +0200

    Added project configuration

--- src/App.js
+++ src/App.js
@@ -9,38 +9,41 @@ class App extends PureComponent {
   timeInterval: IntervalID;
 
   componentDidMount() {
-    this.timeInterval = setInterval(() =>
-    this.setState({time: new Date(), }), 1000);
+    this.timeInterval = setInterval(
+      () => this.setState({ time: new Date() }),
+      1000,
+    );
   }
 
   componentWillUnmount() {
     clearInterval(this.timeInterval);
   }
 
-    render() {
-      const { time } = this.state;
-      let session = 'AM';
-      let hours = time.getHours(),
-          minutes = time.getMinutes(),
-          seconds = time.getSeconds();
+  render() {
+    const { time } = this.state;
+    let session = 'AM';
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
 
-      if ( hours === 0) {
-          hours = 12;
-      }
+    if (hours === 0) {
+      hours = 12;
+    }
 
-      if ( hours > 12) {
-          hours = hours - 12;
-          session = 'PM';
-      }
+    if (hours > 12) {
+      hours -= 12;
+      session = 'PM';
+    }
 
-       seconds = seconds < 10 ? '0' + seconds : seconds;
-       hours = hours < 10 ? '0' + hours : hours;
-       minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
 
-      return (
-
-        <div className="timer">{hours}:{minutes}:{seconds} <span className="session">{session}</span></div>
-      )
+    return (
+      <div className="timer">
+        {hours}:{minutes}:{seconds} <span className="session">{session}</span>
+      </div>
+    );
   }
 }
 
