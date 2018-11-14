7ff040a53dbfe4d89a9aac84f37a9d93ae70950a
Yuriy <yuriy.gavrilyuk@auroraglobal.com>
2018-11-14 19:20:51 +0200

    Webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const webpack = require('webpack');
+const ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const argvs = require('yargs').argv;
 
-const devMode = process.env.NODE_ENV !== 'production';
+const config = {
+  'feature-detects': ['input', 'canvas', 'css/resize'],
+};
+
+const isDevelopment = argvs.mode === 'development';
+const isProduction = !isDevelopment;
 
 module.exports = {
-  //  entry: './src/index.js',
   module: {
     rules: [
       {
@@ -23,11 +34,45 @@ module.exports = {
         ],
       },
       {
-        test: /\.(sa|sc|c)ss$/,
+        test: /\.scss$/,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer({
+                  browsers: ['< 1%'],
+                }),
+                isProduction ? cssnano() : () => {},
+              ],
+            },
+          },
           'sass-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              useRelativePath: true,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              },
+            },
+          },
         ],
       },
     ],
@@ -40,16 +85,37 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {}),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      filename: isDevelopment ? 'style.css' : 'style.min.css',
     }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/images',
+        to: 'images/',
+      },
+    ]),
+    new ModernizrWebpackPlugin(config),
   ],
+  optimization: isProduction
+    ? {
+        minimizer: [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              compress: {
+                inline: false,
+                drop_console: true,
+              },
+            },
+          }),
+        ],
+      }
+    : {},
 
   devServer: {
     contentBase: './dist',
