9c8637bbb4bd334e2dcc92300cb7b4e04b0ce2eb
Yuriy <yuriy.gavrilyuk@auroraglobal.com>
2018-11-15 15:41:48 +0200

    Final version webpack conf

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import DigitalClock from './components/DigitalClock';
 
-class App extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      time: new Date(),
-    };
-  }
-
-  // timeInterval: IntervalID;
-
-  componentDidMount() {
-    this.timeInterval = setInterval(
-      () => this.setState({ time: new Date() }),
-      1000,
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timeInterval);
-  }
-
-  render() {
-    const { time } = this.state;
-    let session = 'AM';
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-
-    if (hours === 0) {
-      hours = 12;
-    }
-
-    if (hours > 12) {
-      hours -= 12;
-      session = 'PM';
-    }
-
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
-    hours = hours < 10 ? `0${hours}` : hours;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-    return (
-      <div className="timer">
-        {hours}:{minutes}:{seconds} <span className="session">{session}</span>
-      </div>
-    );
-  }
+export default function App() {
+  return <DigitalClock />;
 }
-
-export default App;
